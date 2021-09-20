f4a628a7c4a07e2d2e0532d10b3bb5e95212a872
Sakal Mon <sakal.mon@ellumehealth.com>
2021-09-20 13:44:45 +1000

    Initial commit

---
+++ debug.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+ceq_map = {
+    'AR1': {'Line1': 'CEQ175',
+            'Line2': 'CEQ176',
+            'Line3': 'CEQ271',
+            'Line4': 'CEQ275',
+            'Line5': 'CEQ177',
+            'Line6': 'CEQ179'}
+    'AR2': {'Line1': 'CEQ178',
+            'Line2': 'CEQ181',
+            'Line3': 'CEQ180',
+            'Line4': 'CEQ192',
+            'Line5': 'CEQ215',
+            'Line6': 'CEQ214'}
+    'AR3': {'Line1': 'CEQ221',
+            'Line2': 'CEQ212',
+            'Line3': 'CEQ217'}
+}
