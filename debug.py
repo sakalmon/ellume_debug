a090f58946be7f2d104a171c065a191655242723
Sakal Mon <sakal.mon@ellumehealth.com>
2021-09-20 16:45:17 +1000

    Started implementing date check

--- debug.py
+++ debug.py
@@ -1,21 +1,47 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
+
+csv_dir = r'C:\My Drive\Firmware Release\EPL Production Test Results\Tests'
 
 ceq_map = {
-    'AR1': {'Line1': 'CEQ175',
-            'Line2': 'CEQ176',
-            'Line3': 'CEQ271',
-            'Line4': 'CEQ275',
-            'Line5': 'CEQ177',
-            'Line6': 'CEQ179'}
-    'AR2': {'Line1': 'CEQ178',
-            'Line2': 'CEQ181',
-            'Line3': 'CEQ180',
-            'Line4': 'CEQ192',
-            'Line5': 'CEQ215',
-            'Line6': 'CEQ214'}
-    'AR3': {'Line1': 'CEQ221',
-            'Line2': 'CEQ212',
-            'Line3': 'CEQ217'}
+    'AR1': {'Line1': 'CEQ0175',
+            'Line2': 'CEQ0176',
+            'Line3': 'CEQ0271',
+            'Line4': 'CEQ0275',
+            'Line5': 'CEQ0177',
+            'Line6': 'CEQ0179'
+    },
+    'AR2': {'Line1': 'CEQ0178',
+            'Line2': 'CEQ0181',
+            'Line3': 'CEQ0180',
+            'Line4': 'CEQ0192',
+            'Line5': 'CEQ0215',
+            'Line6': 'CEQ0214'
+    },
+    'AR3': {'Line1': 'CEQ0221',
+            'Line2': 'CEQ0212',
+            'Line3': 'CEQ0217'
+    }
 }
+
+ar = 'AR' + input('AR: ')
+line = 'Line' + input('Line: ')
+
+todays_date = datetime.date(datetime.now())
+
+df = pd.DataFrame()
+
+for file in os.listdir(csv_dir):
+    if file.endswith(ceq_map[ar][line] + '.csv'):
+        file_unix_date = int(os.path.getmtime(os.path.join(csv_dir, file)))
+        file_mdate = datetime.date(datetime.fromtimestamp(file_unix_date))
+
+        if file_mdate == todays_date:
+            df = df.append(pd.read_csv(os.path.join(csv_dir, )))
+
+#input()
+#     if file.endswith('.csv'):
+#         df = df.append(pd.read_csv(file, header=2), ignore_index=True)
