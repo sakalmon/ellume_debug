4bf7b25e9c28cb3250d6349ee4c22d8d9ac7f5d3
Sakal Mon <sakal.mon@ellumehealth.com>
2021-09-23 20:01:44 +1000

    Dataframe includes all rooms and lines

--- debug.py
+++ debug.py
@@ -7,69 +7,84 @@ from datetime import date, timedelta
 
 pd.set_option('display.max_columns', None)
 csv_dir = r'C:\My Drive\Firmware Release\EPL Production Test Results\Tests'
-downloads_dir = r'C:\Users\Sakal\Downloads'
+#downloads_dir = r'C:\Users\Sakal\Downloads'
+downloads_dir = r'C:\Users\sakal.mon\Downloads'
 
 # For storing number of fails for each test
 failed_counts = {
-        'AR1': {
-                'Line1'
-        }
-        'Firmware Test': 0,
-        'Serial Test': 0,
-        'Self-Test Test': 0,
-        'Program Test': 0,
-        'Batch Test': 0,
-        'Bluetooth Test': 0,
-        'LED Test': 0,
-        'Button Test': 0,
-        'Metadata Test': 0,
+    'Firmware Test': 0,                
+    'Serial Test': 0,
+    'Self-Test Test': 0,
+    'Program Test': 0,
+    'Batch Test': 0,
+    'Bluetooth Test': 0,
+    'LED Test': 0,
+    'Button Test': 0,
+    'Metadata Test': 0,
 }
 
 # For locating CEQ number
 ceq_map = {
-    'AR1': {'Line1': 'CEQ0175',
-            'Line2': 'CEQ0176',
-            'Line3': 'CEQ0271',
-            'Line4': 'CEQ0275',
-            'Line5': 'CEQ0177',
-            'Line6': 'CEQ0179'
+    'AR1': {
+        'Line1': 'CEQ0175',
+        'Line2': 'CEQ0176',
+        'Line3': 'CEQ0271',
+        'Line4': 'CEQ0275',
+        'Line5': 'CEQ0177',
+        'Line6': 'CEQ0179'
     },
-    'AR2': {'Line1': 'CEQ0178',
-            'Line2': 'CEQ0181',
-            'Line3': 'CEQ0180',
-            'Line4': 'CEQ0192',
-            'Line5': 'CEQ0215',
-            'Line6': 'CEQ0214'
+    'AR2': {
+        'Line1': 'CEQ0178',
+        'Line2': 'CEQ0181',
+        'Line3': 'CEQ0180',
+        'Line4': 'CEQ0192',
+        'Line5': 'CEQ0215',
+        'Line6': 'CEQ0214'
     },
-    'AR3': {'Line1': 'CEQ0221',
-            'Line2': 'CEQ0212',
-            'Line3': 'CEQ0217'
+    'AR3': {
+        'Line1': 'CEQ0221',
+        'Line2': 'CEQ0212',
+        'Line3': 'CEQ0217'
     }
 }
 
-ar = 'AR' + input('AR: ')
-line = 'Line' + input('Line: ')
+# Get room and line number
+def get_input():
+    ar = 'AR' + input('AR: ')
+    line = 'Line' + input('Line: ')
 
+    return (ar, line)
 
 # Today's date will be used to download result files
 todays_date = date.today()
-#todays_date = date.today().strftime('%Y-%m-%d')
 
-print(todays_date)
+#ar, line = get_input()
 
+# Initialise an empty dataframe
 df = pd.DataFrame()
 
+# Initialise subplots for 12 lines
+fig, ax = plt.subplots(12)
+
+#ar, line = get_input()
+
 for file in os.listdir(csv_dir):
-    if file.endswith(ceq_map[ar][line] + '.csv'):
-        file_unix_date = int(os.path.getmtime(os.path.join(csv_dir, file)))
-        file_mdate = date.fromtimestamp(file_unix_date)
+    for ar, lines in ceq_map.items():
+        for line, ceq in lines.items():
+            if file.endswith(ceq + '.csv'):
+                file_unix_date = int(os.path.getmtime(os.path.join(csv_dir, file)))
+                file_mdate = date.fromtimestamp(file_unix_date)
 
-        #if file_mdate == todays_date:
-        shutil.copy(os.path.join(csv_dir, file), downloads_dir)
-        df = df.append(pd.read_csv(os.path.join(downloads_dir, file), header=3, parse_dates=True), ignore_index=True)
+                #if file_mdate == todays_date:
+                file_path = os.path.join(csv_dir, file)
+                shutil.copy(file_path, downloads_dir)
+                downloaded_path = os.path.join(downloads_dir, file)
 
-pickle.dump(df, open('df.pickled', 'wb'))
+                try:
+                    df = df.append(pd.read_csv(downloaded_path, header=2, parse_dates=True), ignore_index=True)
 
+                except:
+                    pass
 # Filter for only failed results
 filt_failed = df['Passed'] == False
 
@@ -83,20 +98,58 @@ df_failed.index = pd.to_datetime(df_failed.index)
 
 # Filter for only the current day
 try:
-        df_failed_today = df_failed.loc[str(todays_date)]
+    df_failed_today = df_failed.loc[str(todays_date)]
 
 except:
-        pass
+    pass
+
+i = 1
+for room, lines in ceq_map.items():
+    for line in lines:
+        print(room, '->', line)
+
+        # Count number of fails per test
+        for key in failed_counts.keys():
+            try: 
+                failed_counts[key] = df_failed_today[key].value_counts()[0]
+            except:
+                pass
+
+        # Sort counts descending
+        failed_counts = dict(sorted(failed_counts.items(), key=lambda item: item[1], reverse=True))
+
+    ax[i].bar(range(len(failed_counts)), failed_counts.values(), width=0.5)
+
+    failed_counts = {
+        'Firmware Test': 0,                
+        'Serial Test': 0,
+        'Self-Test Test': 0,
+        'Program Test': 0,
+        'Batch Test': 0,
+        'Bluetooth Test': 0,
+        'LED Test': 0,
+        'Button Test': 0,
+        'Metadata Test': 0,
+    }
+
+    i += 1
+
+yesterdays_date = todays_date - timedelta(days=1)
+print(yesterdays_date)
 
 # Count number of fails per test
 for key in failed_counts.keys():
-        try: 
-                failed_counts[key] = df_failed_today[key].value_counts()[0]
-        except:
-                pass
+    try: 
+        failed_counts[key] = df_failed_all[key].value_counts()[0]
+    except:
+        pass
 
 # Sort counts descending
 failed_counts = dict(sorted(failed_counts.items(), key=lambda item: item[1], reverse=True))
+
+fig.set_figwidth(15)
+plt.title('Summary of Failures')
+plt.show()
 
 # Plot results
 # plt.bar(range(len(failed_counts)), failed_counts.values(), width=0.5)
@@ -107,30 +160,3 @@ failed_counts = dict(sorted(failed_counts.items(), key=lambda item: item[1], rev
 # plt.ylabel('Occurences')
 # plt.grid(axis='y', linestyle='--')
 # plt.show()
-
-fig, (ax1, ax2) = plt.subplots(2)
-ax1.bar(range(len(failed_counts)), failed_counts.values(), width=0.5)
-
-yesterdays_date = todays_date - timedelta(days=1)
-print(yesterdays_date)
-
-try:
-        df_failed_all = df_failed
-
-except:
-        pass
-
-# Count number of fails per test
-for key in failed_counts.keys():
-        try: 
-                failed_counts[key] = df_failed_all[key].value_counts()[0]
-        except:
-                pass
-
-# Sort counts descending
-failed_counts = dict(sorted(failed_counts.items(), key=lambda item: item[1], reverse=True))
-
-ax2.bar(range(len(failed_counts)), failed_counts.values(), width=0.5)
-fig.set_figwidth(15)
-plt.title('Summary of Failures')
-plt.show()
